af662252486e226c6160ff96b742ecbc8e6923a5
Mart Pluijmaekers <mart@gewis.nl>
2014-12-30 15:55:17 +0100

    Initial push of Jeroens code

---
+++ VolVis/src/volvis/RaycastRenderer.java
@@ -0,0 +1,246 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package volvis;
+
+import com.jogamp.opengl.util.texture.Texture;
+import com.jogamp.opengl.util.texture.awt.AWTTextureIO;
+import gui.RaycastRendererPanel;
+import gui.TransferFunctionEditor;
+import java.awt.image.BufferedImage;
+import javax.media.opengl.GL2;
+import util.TFChangeListener;
+import util.VectorMath;
+import volume.Volume;
+
+/**
+ *
+ * @author michel
+ */
+public class RaycastRenderer extends Renderer implements TFChangeListener {
+
+    private Volume volume = null;
+    RaycastRendererPanel panel;
+    TransferFunction tFunc;
+    TransferFunctionEditor tfEditor;
+
+    public RaycastRenderer() {
+        panel = new RaycastRendererPanel(this);
+        panel.setSpeedLabel("0");
+    }
+
+    public void setVolume(Volume vol) {
+        volume = vol;
+
+        // set up image for storing the resulting rendering
+        // the image width and height are equal to the length of the volume diagonal
+        int imageSize = (int) Math.floor(Math.sqrt(vol.getDimX() * vol.getDimX() + vol.getDimY() * vol.getDimY()
+                + vol.getDimZ() * vol.getDimZ()));
+        if (imageSize % 2 != 0) {
+            imageSize = imageSize + 1;
+        }
+        image = new BufferedImage(imageSize, imageSize, BufferedImage.TYPE_INT_ARGB);
+        tFunc = new TransferFunction(volume.getMinimum(), volume.getMaximum());
+        tFunc.addTFChangeListener(this);
+        tfEditor = new TransferFunctionEditor(tFunc, volume.getHistogram());
+        panel.setTransferFunctionEditor(tfEditor);
+
+
+    }
+
+    @Override
+    public void changed() {
+        for (int i = 0; i < listeners.size(); i++) {
+            listeners.get(i).changed();
+        }
+    }
+
+    public RaycastRendererPanel getPanel() {
+        return panel;
+    }
+
+    // get a voxel from the volume data by nearest neighbor interpolation
+    short getVoxel(double[] coord) {
+
+        int x = (int) Math.round(coord[0]);
+        int y = (int) Math.round(coord[1]);
+        int z = (int) Math.round(coord[2]);
+
+        if ((x >= 0) && (x < volume.getDimX()) && (y >= 0) && (y < volume.getDimY())
+                && (z >= 0) && (z < volume.getDimZ())) {
+            return volume.getVoxel(x, y, z);
+        } else {
+            return 0;
+        }
+    }
+
+    void slicer(double[] viewMatrix) {
+
+        // clear image
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
+                image.setRGB(i, j, 0);
+            }
+        }
+
+        // vector uVec and vVec define a plane through the origin, 
+        // perpendicular to the view vector viewVec
+        double[] viewVec = new double[3];
+        double[] uVec = new double[3];
+        double[] vVec = new double[3];
+        VectorMath.setVector(viewVec, viewMatrix[2], viewMatrix[6], viewMatrix[10]);
+        VectorMath.setVector(uVec, viewMatrix[0], viewMatrix[4], viewMatrix[8]);
+        VectorMath.setVector(vVec, viewMatrix[1], viewMatrix[5], viewMatrix[9]);
+
+        // image is square
+        int imageCenter = image.getWidth() / 2;
+
+        double[] pixelCoord = new double[3];
+        double[] volumeCenter = new double[3];
+        VectorMath.setVector(volumeCenter, volume.getDimX() / 2, volume.getDimY() / 2, volume.getDimZ() / 2);
+
+        // sample on a plane through the origin of the volume data
+        double max = volume.getMaximum();
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
+                pixelCoord[0] = uVec[0] * (i - imageCenter) + vVec[0] * (j - imageCenter)
+                        + volumeCenter[0];
+                pixelCoord[1] = uVec[1] * (i - imageCenter) + vVec[1] * (j - imageCenter)
+                        + volumeCenter[1];
+                pixelCoord[2] = uVec[2] * (i - imageCenter) + vVec[2] * (j - imageCenter)
+                        + volumeCenter[2];
+
+                int val = getVoxel(pixelCoord);
+                // Apply the transfer function to obtain a color
+                TFColor voxelColor = tFunc.getColor(val);
+                
+                // BufferedImage expects a pixel color packed as ARGB in an int
+                int c_alpha = voxelColor.a <= 1.0 ? (int) Math.floor(voxelColor.a * 255) : 255;
+                int c_red = voxelColor.r <= 1.0 ? (int) Math.floor(voxelColor.r * 255) : 255;
+                int c_green = voxelColor.g <= 1.0 ? (int) Math.floor(voxelColor.g * 255) : 255;
+                int c_blue = voxelColor.b <= 1.0 ? (int) Math.floor(voxelColor.b * 255) : 255;
+                int pixelColor = (c_alpha << 24) | (c_red << 16) | (c_green << 8) | c_blue;
+                image.setRGB(i, j, pixelColor);
+            }
+        }
+
+
+    }
+
+    private void drawBoundingBox(GL2 gl) {
+        gl.glPushAttrib(GL2.GL_CURRENT_BIT);
+        gl.glDisable(GL2.GL_LIGHTING);
+        gl.glColor4d(1.0, 1.0, 1.0, 1.0);
+        gl.glLineWidth(1.5f);
+        gl.glEnable(GL2.GL_LINE_SMOOTH);
+        gl.glHint(GL2.GL_LINE_SMOOTH_HINT, GL2.GL_NICEST);
+        gl.glEnable(GL2.GL_BLEND);
+        gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+        gl.glVertex3d(-volume.getDimX() / 2.0, -volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, -volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glEnd();
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+        gl.glVertex3d(-volume.getDimX() / 2.0, -volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, -volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glEnd();
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+        gl.glVertex3d(volume.getDimX() / 2.0, -volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, -volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glEnd();
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+        gl.glVertex3d(-volume.getDimX() / 2.0, -volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, -volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glEnd();
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+        gl.glVertex3d(-volume.getDimX() / 2.0, volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glEnd();
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+        gl.glVertex3d(-volume.getDimX() / 2.0, -volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, -volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, -volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, -volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glEnd();
+
+        gl.glDisable(GL2.GL_LINE_SMOOTH);
+        gl.glDisable(GL2.GL_BLEND);
+        gl.glEnable(GL2.GL_LIGHTING);
+        gl.glPopAttrib();
+
+    }
+
+    @Override
+    public void visualize(GL2 gl) {
+
+
+        if (volume == null) {
+            return;
+        }
+
+        drawBoundingBox(gl);
+
+        gl.glGetDoublev(GL2.GL_MODELVIEW_MATRIX, viewMatrix, 0);
+
+        long startTime = System.currentTimeMillis();
+        slicer(viewMatrix);
+        long endTime = System.currentTimeMillis();
+        double runningTime = (endTime - startTime);
+        panel.setSpeedLabel(Double.toString(runningTime));
+
+        Texture texture = AWTTextureIO.newTexture(gl.getGLProfile(), image, false);
+
+        gl.glPushAttrib(GL2.GL_LIGHTING_BIT);
+        gl.glDisable(GL2.GL_LIGHTING);
+        gl.glEnable(GL2.GL_BLEND);
+        gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
+
+        // draw rendered image as a billboard texture
+        texture.enable(gl);
+        texture.bind(gl);
+        double halfWidth = image.getWidth() / 2.0;
+        gl.glPushMatrix();
+        gl.glLoadIdentity();
+        gl.glBegin(GL2.GL_QUADS);
+        gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        gl.glTexCoord2d(0.0, 0.0);
+        gl.glVertex3d(-halfWidth, -halfWidth, 0.0);
+        gl.glTexCoord2d(0.0, 1.0);
+        gl.glVertex3d(-halfWidth, halfWidth, 0.0);
+        gl.glTexCoord2d(1.0, 1.0);
+        gl.glVertex3d(halfWidth, halfWidth, 0.0);
+        gl.glTexCoord2d(1.0, 0.0);
+        gl.glVertex3d(halfWidth, -halfWidth, 0.0);
+        gl.glEnd();
+        texture.disable(gl);
+        texture.destroy(gl);
+        gl.glPopMatrix();
+
+        gl.glPopAttrib();
+
+
+        if (gl.glGetError() > 0) {
+            System.out.println("some OpenGL error: " + gl.glGetError());
+        }
+
+    }
+    private BufferedImage image;
+    private double[] viewMatrix = new double[4 * 4];
+}
---
+++ VolVis/src/volvis/TFRenderer.java
@@ -0,0 +1,286 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package volvis;
+
+import com.jogamp.opengl.util.texture.Texture;
+import com.jogamp.opengl.util.texture.awt.AWTTextureIO;
+import gui.MIPRendererPanel;
+import gui.RaycastRendererPanel;
+import gui.TransferFunctionEditor;
+import java.awt.image.BufferedImage;
+import javax.media.opengl.GL2;
+import util.TFChangeListener;
+import util.VectorMath;
+import volume.Volume;
+
+/**
+ *
+ * @author michel
+ */
+public class TFRenderer extends Renderer implements TFChangeListener {
+
+    private Volume volume = null;
+    MIPRendererPanel panel;
+    TransferFunction tFunc;
+    TransferFunctionEditor tfEditor;
+
+    public TFRenderer(Visualization v) {
+        panel = new MIPRendererPanel(this, v);
+        panel.setSpeedLabel("0");
+    }
+
+    public void setVolume(Volume vol) {
+        volume = vol;
+
+        // set up image for storing the resulting rendering
+        // the image width and height are equal to the length of the volume diagonal
+        int imageSize = (int) Math.floor(Math.sqrt(vol.getDimX() * vol.getDimX() + vol.getDimY() * vol.getDimY()
+                + vol.getDimZ() * vol.getDimZ()));
+        if (imageSize % 2 != 0) {
+            imageSize = imageSize + 1;
+        }
+        image = new BufferedImage(imageSize, imageSize, BufferedImage.TYPE_INT_ARGB);
+        tFunc = new TransferFunction(volume.getMinimum(), volume.getMaximum());
+        tFunc.addTFChangeListener(this);
+        tfEditor = new TransferFunctionEditor(tFunc, volume.getHistogram());
+        panel.setTransferFunctionEditor(tfEditor);
+        panel.Samples.setValue(volume.getDimZ() / 10);
+
+
+    }
+
+    @Override
+    public void changed() {
+        for (int i = 0; i < listeners.size(); i++) {
+            listeners.get(i).changed();
+        }
+    }
+
+    public MIPRendererPanel getPanel() {
+        return panel;
+    }
+
+    // get a voxel from the volume data by nearest neighbor interpolation
+    short getVoxel(double[] coord) {
+        short[] S = new short[8];
+        double a = 0, b = 0, g = 0;
+
+
+
+        if ((Math.floor(coord[0]) >= 0) && (Math.ceil(coord[0]) < volume.getDimX()) && (Math.floor(coord[1]) >= 0) && (Math.ceil(coord[1]) < volume.getDimY())
+                && (Math.floor(coord[2]) >= 0) && (Math.ceil(coord[2]) < volume.getDimZ())) {
+
+            for (int i = 0; i < 8; i++) {
+                int x, y, z;
+                if (i % 2 == 0) {
+                    x = (int) Math.floor(coord[0]);
+                } else {
+                    x = (int) Math.ceil(coord[0]);
+                }
+                if (i % 4 == 0 || i % 4 == 1) {
+                    y = (int) Math.floor(coord[1]);
+                } else {
+                    y = (int) Math.ceil(coord[1]);
+                }
+                if (i < 4) {
+                    z = (int) Math.floor(coord[2]);
+                } else {
+                    z = (int) Math.ceil(coord[2]);
+                }
+                if (i == 0) {
+                    a = coord[0] - x;
+                    b = coord[1] - y;
+                    g = coord[2] - z;
+                }
+                S[i] = volume.getVoxel(x, y, z);
+            }
+            return (short) ((1 - a) * (1 - b) * (1 - g) * S[0] + a * (1 - b) * (1 - g) * S[1]
+                    + (1 - a) * b * (1 - g) * S[2] + a * b * (1 - g) * S[3]
+                    + (1 - a) * (1 - b) * g * S[4] + a * (1 - b) * g * S[5]
+                    + (1 - a) * b * g * S[6] + a * b * g * S[7]);
+        } else {
+            return 0;
+        }
+    }
+
+    void slicer(double[] viewMatrix) {
+
+        // clear image
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
+                image.setRGB(i, j, 0);
+            }
+        }
+
+        // vector uVec and vVec define a plane through the origin, 
+        // perpendicular to the view vector viewVec
+        double[] viewVec = new double[3];
+        double[] uVec = new double[3];
+        double[] vVec = new double[3];
+        VectorMath.setVector(viewVec, viewMatrix[2], viewMatrix[6], viewMatrix[10]);
+        VectorMath.setVector(uVec, viewMatrix[0], viewMatrix[4], viewMatrix[8]);
+        VectorMath.setVector(vVec, viewMatrix[1], viewMatrix[5], viewMatrix[9]);
+
+        // image is square
+        int imageCenter = image.getWidth() / 2;
+
+        double[] pixelCoord = new double[3];
+        double[] volumeCenter = new double[3];
+        VectorMath.setVector(volumeCenter, volume.getDimX() / 2, volume.getDimY() / 2, volume.getDimZ() / 2);
+
+        // sample on a plane through the origin of the volume data
+        double max = volume.getMaximum();
+        TFColor voxelColor = tFunc.getColor(0);
+        int c_alpha = voxelColor.a <= 1.0 ? (int) Math.floor(voxelColor.a * 255) : 255;
+        int c_red = voxelColor.r <= 1.0 ? (int) Math.floor(voxelColor.r * 255) : 255;
+        int c_green = voxelColor.g <= 1.0 ? (int) Math.floor(voxelColor.g * 255) : 255;
+        int c_blue = voxelColor.b <= 1.0 ? (int) Math.floor(voxelColor.b * 255) : 255;
+        for (int j = 0; j < image.getHeight(); j += 1) {
+            for (int i = 0; i < image.getWidth(); i += 1) {
+                int val = 0;
+                String stupidval = panel.Samples.getValue().toString();
+                for (double k = 0; k < volume.getDimZ(); k += (volume.getDimZ() / (Integer.parseInt(stupidval) * 1.0))) {
+
+                    pixelCoord[0] = uVec[0] * (i - imageCenter) + vVec[0] * (j - imageCenter)
+                            + viewVec[0] * (k) + volumeCenter[0];
+                    pixelCoord[1] = uVec[1] * (i - imageCenter) + vVec[1] * (j - imageCenter)
+                            + viewVec[1] * (k) + volumeCenter[1];
+                    pixelCoord[2] = uVec[2] * (i - imageCenter) + vVec[2] * (j - imageCenter)
+                            + viewVec[2] * (k) + volumeCenter[2];
+
+                    voxelColor = tFunc.getColor(getVoxel(pixelCoord));
+                    c_alpha = (voxelColor.a <= 1.0 ? (int) Math.floor(voxelColor.a * 255) : 255)+(int)(1-(getVoxel(pixelCoord)*1.0/max))*c_alpha;
+                    c_red = (voxelColor.r <= 1.0 ? (int) Math.floor(voxelColor.r * 255) : 255)+(int)(1-(getVoxel(pixelCoord)*1.0/max))*c_red;
+                    c_green = (voxelColor.g <= 1.0 ? (int) Math.floor(voxelColor.g * 255) : 255)+(int)(1-(getVoxel(pixelCoord)*1.0/max))*c_green;
+                    c_blue = (voxelColor.b <= 1.0 ? (int) Math.floor(voxelColor.b * 255) : 255)+(int)(1-(getVoxel(pixelCoord)*1.0/max))*c_blue;
+//                    val = (int) (+(1 - ((getVoxel(pixelCoord) * 1.0) / max)) * val);
+                }
+
+                // BufferedImage expects a pixel color packed as ARGB in an int
+
+                int pixelColor = (c_alpha << 24) | (c_red << 16) | (c_green << 8) | c_blue;
+                image.setRGB(i, j, pixelColor);
+            }
+        }
+
+
+    }
+
+    private void drawBoundingBox(GL2 gl) {
+        gl.glPushAttrib(GL2.GL_CURRENT_BIT);
+        gl.glDisable(GL2.GL_LIGHTING);
+        gl.glColor4d(1.0, 1.0, 1.0, 1.0);
+        gl.glLineWidth(1.5f);
+        gl.glEnable(GL2.GL_LINE_SMOOTH);
+        gl.glHint(GL2.GL_LINE_SMOOTH_HINT, GL2.GL_NICEST);
+        gl.glEnable(GL2.GL_BLEND);
+        gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+        gl.glVertex3d(-volume.getDimX() / 2.0, -volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, -volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glEnd();
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+        gl.glVertex3d(-volume.getDimX() / 2.0, -volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, -volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glEnd();
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+        gl.glVertex3d(volume.getDimX() / 2.0, -volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, -volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glEnd();
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+        gl.glVertex3d(-volume.getDimX() / 2.0, -volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, -volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glEnd();
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+        gl.glVertex3d(-volume.getDimX() / 2.0, volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glEnd();
+
+        gl.glBegin(GL2.GL_LINE_LOOP);
+        gl.glVertex3d(-volume.getDimX() / 2.0, -volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glVertex3d(-volume.getDimX() / 2.0, -volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, -volume.getDimY() / 2.0, volume.getDimZ() / 2.0);
+        gl.glVertex3d(volume.getDimX() / 2.0, -volume.getDimY() / 2.0, -volume.getDimZ() / 2.0);
+        gl.glEnd();
+
+        gl.glDisable(GL2.GL_LINE_SMOOTH);
+        gl.glDisable(GL2.GL_BLEND);
+        gl.glEnable(GL2.GL_LIGHTING);
+        gl.glPopAttrib();
+
+    }
+
+    @Override
+    public void visualize(GL2 gl) {
+
+
+        if (volume == null) {
+            return;
+        }
+
+        drawBoundingBox(gl);
+
+        gl.glGetDoublev(GL2.GL_MODELVIEW_MATRIX, viewMatrix, 0);
+
+        long startTime = System.currentTimeMillis();
+        slicer(viewMatrix);
+        long endTime = System.currentTimeMillis();
+        double runningTime = (endTime - startTime);
+        panel.setSpeedLabel(Double.toString(runningTime));
+
+        Texture texture = AWTTextureIO.newTexture(gl.getGLProfile(), image, false);
+
+        gl.glPushAttrib(GL2.GL_LIGHTING_BIT);
+        gl.glDisable(GL2.GL_LIGHTING);
+        gl.glEnable(GL2.GL_BLEND);
+        gl.glBlendFunc(GL2.GL_SRC_ALPHA, GL2.GL_ONE_MINUS_SRC_ALPHA);
+
+        // draw rendered image as a billboard texture
+        texture.enable(gl);
+        texture.bind(gl);
+        double halfWidth = image.getWidth() / 2.0;
+        gl.glPushMatrix();
+        gl.glLoadIdentity();
+        gl.glBegin(GL2.GL_QUADS);
+        gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        gl.glTexCoord2d(0.0, 0.0);
+        gl.glVertex3d(-halfWidth, -halfWidth, 0.0);
+        gl.glTexCoord2d(0.0, 1.0);
+        gl.glVertex3d(-halfWidth, halfWidth, 0.0);
+        gl.glTexCoord2d(1.0, 1.0);
+        gl.glVertex3d(halfWidth, halfWidth, 0.0);
+        gl.glTexCoord2d(1.0, 0.0);
+        gl.glVertex3d(halfWidth, -halfWidth, 0.0);
+        gl.glEnd();
+        texture.disable(gl);
+        texture.destroy(gl);
+        gl.glPopMatrix();
+
+        gl.glPopAttrib();
+
+
+        if (gl.glGetError() > 0) {
+            System.out.println("some OpenGL error: " + gl.glGetError());
+        }
+
+    }
+    private BufferedImage image;
+    private double[] viewMatrix = new double[4 * 4];
+}
